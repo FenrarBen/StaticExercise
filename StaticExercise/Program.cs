ff1357ab2873931f16f447397968baece43583f2
BenStrawbridge <lute_midnight@yahoo.com>
2020-11-05 10:58:01 -0600

    Static Class Exercise

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("32 F is " + TempConverter.FahrenheitToCelsius(32) + " C");
+            Console.WriteLine("100 C is " + TempConverter.CelsiusToFahrenheit(100) + " F");
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        static TempConverter()
+        {
+
+        }
+
+        static public double FahrenheitToCelsius(double temp)
+        {
+            return ((temp - 32.0) * (5.0/9.0));
+        }
+
+        static public double CelsiusToFahrenheit(double temp)
+        {
+            return (temp * (9.0 / 5.0)) + 32.0;
+        }
+    }
+}
